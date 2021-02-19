c7f97bbd1a4ad347e50883b17fc1529c8fea8d9c
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-02-19 03:52:41 +0300

    Add DynamicArray

---
+++ DynamicArray/src/DynamicArray.java
@@ -0,0 +1,33 @@
+public class DynamicArray<T> {
+    private void throwException() {
+        throw new ArithmeticException("Слышь тебе сюда нельзя");
+} public int size(){
+        return array.length;
+    }
+        private final int DEFAULT_SIZE = 10;
+        private T[] array;
+
+        public DynamicArray(){
+            array = (T[])new Object[DEFAULT_SIZE];
+        }
+
+        public T get(int index){
+            if(size() < index | index < 0){throwException();}
+            return array[index];
+        }
+
+        public void set(int index,T value){
+            if(size() < index | index < 0){throwException();}
+            array[index] = value;
+        }
+        public void resize(int newSize){
+            if (newSize < 0){throwException();}
+            T[] newArray = array.clone();
+            array = (T[])new Object[newSize];
+            for(int i = 0; i < newSize; i++){
+                array[i] = newArray[i];
+            }
+        }
+}
+
+
